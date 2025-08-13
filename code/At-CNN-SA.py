e306da7fce29238728ca3f7452027d1c82367700
paofan666 <2759027453@qq.com>
2025-08-13 21:33:04 +0800

    Update At-CNN-SA.py

--- code/At-CNN-SA.py
+++ code/At-CNN-SA.py
@@ -11,7 +11,7 @@ from sklearn import metrics
 
 train_data = pd.read_csv('D:/Code/java/veloctiy_data/train_composite_data.csv')
 test_data = pd.read_csv('D:/Code/java/veloctiy_data/test_composite_data.csv')
-#读取训练和测试的X和Y
+#Read the X and Y of training and testing
 x_train = train_data.iloc[:, 1:41]
 x_test = test_data.iloc[:, 1:41]
 y_train = train_data.iloc[:, -1]
@@ -21,41 +21,41 @@ smote = SMOTE(k_neighbors=5, random_state=42)
 x_train_resampled, y_train_resampled = smote.fit_resample(x_train, y_train)
 
 x_train, x_valid, y_train, y_valid = train_test_split(x_train_resampled, y_train_resampled, random_state=11)
-#修改y的类型
+#Modify the type of y
 y_train = K.cast_to_floatx(y_train)
 y_test = K.cast_to_floatx(y_test)
 y_valid = K.cast_to_floatx(y_valid)
-#数据标准化
+#Data standardization
 scaler = StandardScaler()
 x_train_scaler = scaler.fit_transform(x_train)
 x_test_scaler = scaler.fit_transform(x_test)
 x_valid_scaler = scaler.fit_transform(x_valid)
-#数据缩放至[0,1]
+#Data scaling to [0,1]
 max_abs_scaler = MaxAbsScaler()
 x_train = max_abs_scaler.fit_transform(x_train_scaler)
 x_test = max_abs_scaler.fit_transform(x_test_scaler)
 x_valid = max_abs_scaler.fit_transform(x_valid_scaler)
 
-#将二维数据转换为三维数据(np数据）
+#Converting 2D data to 3D data (NP data)
 x_train = x_train[:, np.newaxis, :]
 x_test = x_test[:, np.newaxis, :]
 x_valid = x_valid[:, np.newaxis, :]
 
 
-# 构建带有自注意力层的卷积神经网络模型
+# Construct a convolutional neural network model with a self-attention layer
 def build_cnn_with_attention(input_shape):
     inputs = tf.keras.layers.Input(shape=input_shape)
     x = tf.keras.layers.Conv1D(64, 1, activation='relu')(inputs)
     x = tf.keras.layers.GlobalMaxPooling1D()(x)
-    # 添加自注意力层
+    # Add a self-attention layer
     attention = tf.keras.layers.Attention()([x, x])
-    # 将自注意力层的输出与原始输出连接起来
+    # Connect the output of the self-attention layer with the original output
     x = tf.keras.layers.Concatenate()([x, attention])
     outputs = tf.keras.layers.Dense(1, activation='sigmoid')(x)  # 二分类问题的输出层
     model = tf.keras.Model(inputs, outputs)
     return model
 
-# 构建模型
+# Build a model
 input_shape = (x_train.shape[1], x_train.shape[2])
 model = build_cnn_with_attention(input_shape)
 optimizer = tf.keras.optimizers.Adam(learning_rate=0.0002)
